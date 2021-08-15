95728b4103bc5a545c182bb13027793bbdf5d207
andy-thomason <andy@andythomason.com>
2021-08-16 00:49:31 +0100

    rebase on master

--- extendr-api/src/wrapper/mod.rs
+++ extendr-api/src/wrapper/mod.rs
@@ -8,6 +8,7 @@ pub mod altrep;
 pub mod character;
 pub mod environment;
 pub mod expr;
+pub mod externalptr;
 pub mod function;
 pub mod lang;
 pub mod list;
@@ -26,6 +27,7 @@ pub use altrep::{
 pub use character::Character;
 pub use environment::{EnvIter, Environment};
 pub use expr::Expression;
+pub use externalptr::ExternalPtr;
 pub use function::Function;
 pub use lang::Language;
 pub use list::{FromList, List, ListIter};
